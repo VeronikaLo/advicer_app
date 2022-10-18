121b9523d1eba99f2e99f823848677d13a2b336d
Veronika L <verokipri@yahoo.com>
2022-10-18 18:38:40 +0200

    custom button

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 
+import 'package:advicer/presentation/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,14 +7,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //final themeData = Theme.of(context);
     return Scaffold(
       appBar:  AppBar(title: const Text('Advicer'), centerTitle: true,),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50),
-        child: Column(
-          children: [
-            
-          ],)
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text("Your Advice is waiting for you!", 
+                  style: Theme.of(context).textTheme.button , ),),
+              ),
+              SizedBox(
+                height: 200,
+                child: Center(child: CustomButton(onPressed:(){} ,)),
+              ),
+            ],)
+        ),
       ),
     );
   }
---
+++ lib/presentation/widgets/custom_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final Function onPressed;
+  const CustomButton({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkResponse(
+      onTap:()=> onPressed,
+      radius: 5 ,
+      child: Material(
+        elevation: 20,
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(20),
+        ),
+          child:  Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Text('Get Advice', style: Theme.of(context).textTheme.button,) ),
+        ),
+      ),
+    );
+  }
+}
