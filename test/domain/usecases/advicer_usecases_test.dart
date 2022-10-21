74675435d6eb3b1c3f3739f3f1298fffa0ddb7dc
Veronika L <verokipri@yahoo.com>
2022-10-22 00:47:11 +0200

    domain layer test

--- test/domain/usecases/advicer_usecases_test.dart
+++ test/domain/usecases/advicer_usecases_test.dart
@@ -1,8 +1,12 @@
 
+import 'package:advicer/domain/entities/advicer_entity.dart';
+import 'package:advicer/domain/failures/failure.dart';
 import 'package:advicer/domain/repositories/advicer_repository.dart';
 import 'package:advicer/domain/usecases/advicer_usecases.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'advicer_usecases_test.mocks.dart';
 
@@ -17,7 +21,42 @@ void main(){
     advicerUsecases = AdvicerUsecases(advicerRepository: mockAdvicerRepository);
   });
 
+  group('getAdviceUsecases', (){
+
+    final t_Advice = AdviceEntity(advice: 'test', id: 1);
+
+  test('should return the same advice as repo', ()async{
+
+    //arrange
+      when(mockAdvicerRepository.getAdviceFromApi()).thenAnswer((_) async =>Right(t_Advice));
+    
+    //act 
+      final result = await advicerUsecases.getAdviceUsecase();
+
+    //assert
+      expect(result, Right(t_Advice));
+      verify(mockAdvicerRepository.getAdviceFromApi());
+      verifyNoMoreInteractions(mockAdvicerRepository);
+  });
+
+  // 2. test:
+
+  test('should return the same ServerFailure', ()async{
+
+    //arrange
+      when(mockAdvicerRepository.getAdviceFromApi()).thenAnswer((_) async =>Left(ServerFailure()));
+    
+    //act 
+      final result = await advicerUsecases.getAdviceUsecase();
+
+    //assert
+      expect(result, Left(ServerFailure()));
+      verify(mockAdvicerRepository.getAdviceFromApi());
+      verifyNoMoreInteractions(mockAdvicerRepository);
+  });
 
 
+  });
 
+  
 }
