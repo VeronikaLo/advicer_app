9b84b596edf28f32cec0ec0caf512b0153dd26f1
Veronika L <verokipri@yahoo.com>
2022-10-21 23:56:22 +0200

    setUp changes

--- test/domain/usecases/advicer_usecases_test.dart
+++ test/domain/usecases/advicer_usecases_test.dart
@@ -1,8 +1,23 @@
 
 import 'package:advicer/domain/repositories/advicer_repository.dart';
+import 'package:advicer/domain/usecases/advicer_usecases.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+
+import 'advicer_usecases_test.mocks.dart';
 
 @GenerateMocks([AdvicerRepository])
 void main(){
+
+  late AdvicerUsecases advicerUsecases;
+  late MockAdvicerRepository mockAdvicerRepository;
+
+  setUp((){
+    mockAdvicerRepository = MockAdvicerRepository();
+    advicerUsecases = AdvicerUsecases(advicerRepository: mockAdvicerRepository);
+  });
+
+
+
 
 }
