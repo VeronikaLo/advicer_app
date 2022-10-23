bce861cd4dfacf1d483da0fec4a8731806423475
Veronika L <verokipri@yahoo.com>
2022-10-23 23:47:29 +0200

    bloc testing start

---
+++ test/application/advice_bloc_test.dart
@@ -0,0 +1,9 @@
+
+import 'package:advicer/domain/usecases/advicer_usecases.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+@GenerateMocks([AdvicerUsecases])
+void main(){
+
+}
