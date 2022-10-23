557c7d2b26666d43be0dbeb5282de01b9ae88a33
Veronika L <verokipri@yahoo.com>
2022-10-23 22:18:20 +0200

    model testing

--- test/data/models/advice_model_test.dart
+++ test/data/models/advice_model_test.dart
@@ -1,7 +1,11 @@
 
+import 'dart:convert';
+
 import 'package:advicer/data/models/advicer_model.dart';
 import 'package:advicer/domain/entities/advicer_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../fixtures/fixture_reader.dart';
 
 void main(){
   //weil wir keine dependency hier haben, brauchen wir kein setUp() und keine Zuweisung, wie machen sofort ein Test-Model
@@ -11,6 +15,19 @@ void main(){
   test('model should be subcluss of advice-entity ', (){
     //assert
     expect(t_AdviceModel, isA<AdviceEntity>());
+  });
+
+  group('fromJson factory', (){
+    test('should return a valid model if the JSON advice is correct', (){
+      //arrange
+      final Map<String, dynamic> jsonMap = jsonDecode(fixture('advice.json'));
+
+      //act
+      final result = AdviceModel.fromJson(jsonMap);
+
+      //assert
+      expect(result, t_AdviceModel);
+    });
   });
 
 }
